3ed94e7d6322dea85ace68bf448524a5d613b2a1
mkmelen <mkmelen@ucsc.edu>
2022-12-12 20:10:24 -0800

    Made more figures

--- Code/borr_y1_habitat/seed_behavior_coxme.R
+++ Code/borr_y1_habitat/seed_behavior_coxme.R
@@ -41,7 +41,7 @@ str(mydata) #Check that each column has the right class (factor, integer, numeri
 hist(mydata$DaysToGerm,col='steelblue',main='Original') 
 
 #By soil type
-ggplot(mydata,aes(x=DaysToGerm))+geom_histogram()+facet_wrap(vars(DishType)) #Here we see that Dittrichia germination responds similarly to both soil types (DishType).
+ggplot(mydata,aes(x=DaysToGerm))+geom_histogram()+facet_wrap(vars(Treatment)) #Here we see that Dittrichia germination responds similarly to both soil types (Treatment).
 
 ####Cox Model: Construction Fill and Forest Soils####
 #Start by making a simple model with no random effects. This will be compared to the full model with random effects.
@@ -59,7 +59,7 @@ predict(fullmodel1)
 hist(predict(fullmodel1))
 
 #Now we can compare the models to see which model is best
-anova(simplemodel1,fullmodel1) 
+anova(simplemodel1,fullmodel1) #There is a significance between models
 #See example: https://www.rdocumentation.org/packages/coxme/versions/2.2-16/topics/coxme
 AIC(simplemodel1,fullmodel1) #But comparing the AIC scores is easiest. Keep the lower AIC score because that is considered the better model
 #              df      AIC
@@ -73,24 +73,37 @@ summary(fullmodel2)
 
 #Let's compare the the first two models to test for the significance of the term that is removed (using LR)
 anova(fullmodel1,fullmodel2) #Not significant
-AIC(fullmodel1,fullmodel2) #Interaction term is not significant! So we can drop the interaction term and keep fullmodel2
+AIC(fullmodel1,fullmodel2) #The two models are within 2 points of each other, so I can use the simpler model, which means the interaction term is dropped and we keep fullmodel2.
+
+#Here's the model we keep:
+summary(fullmodel2)
+#Fixed coefficients
+#                  coef          exp(coef)   se(coef)     z      p
+#HabitatRoadside   0.040964779   1.041815    0.15442210   0.27   0.79
+#TreatmentForest   0.002252879   1.002255    0.05599009   0.04   0.97
 
 ####Risk Assessment####
 #Let's pull out some coefficients, which tell us about the risk of death. 1 = no effect, <1 = decreased risk of death, >1 = increased risk of death.
 
 exp(coef(fullmodel2))
-# HabitatRoadside TreatmentForest 
-#1.041815        1.002255 
-#Both are just above 1, which means that roadside has an ever so so slight increase in germination compared to off-road, and forest has an even smaller slight increase in germination compared to construction.
+# HabitatRoadside  TreatmentForest 
+#1.041815          1.002255 
+#Note that these are also found in the summary(model). When we run this, both are just above 1, which means that roadside has an ever so so slight increase in germination compared to off-road, and forest has an even smaller slight increase in germination compared to construction.
 
 #Here we pull the std dev of the random effects which works like the fixed effect coefficients to calculate risk.
 exp(ranef(fullmodel2)$Site) # Pretty good for most, CHE is lower
 #BAY       CHE       GUA       LEX       OAP       PAR       PEN       SSJ 
 #0.9993086 1.0030391 0.9969110 0.9997749 0.9987377 0.9998305 1.0027064 0.9997059 
 
-exp(ranef(fullmodel2)$Site/Population/DishNum) # Pretty good for most, G and J are lower
-
 ####Plots####
 #Plot by habitat
 model_graph1<-survfit(Surv(DaysToGerm,Censor)~Habitat,data=mydata)
 autoplot(model_graph1)+labs(x="\n Time to Germination (Days)",y="Germination Probabilities\n",title="Germination Times Of Seeds from \n Roadside and Adjacent Vegetation\n")+theme(plot.title=element_text(hjust=0.5),axis.title.x=element_text(face="bold",color="Black",size = 12),axis.title.y=element_text(face="bold",color="Black",size=12),legend.title=element_text(face="bold",size=10))
+
+#Plot by habitat and treatment
+model_graph2<-survfit(Surv(DaysToGerm,Censor)~Habitat+Treatment,data=mydata)
+autoplot(model_graph2)+labs(x="\n Time to Germination (Days)",y="Germination Probabilities\n",title="Germination Times Of Seeds from \n Roadside and Adjacent Vegetation\n")+theme(plot.title=element_text(hjust=0.5),axis.title.x=element_text(face="bold",color="Black",size = 12),axis.title.y=element_text(face="bold",color="Black",size=12),legend.title=element_text(face="bold",size=10))
+
+#Plot by site and treatment
+model_graph3<-survfit(Surv(DaysToGerm,Censor)~Site+Treatment,data=mydata)
+autoplot(model_graph3)+labs(x="\n Time to Germination (Days)",y="Germination Probabilities\n",title="Germination Times Of Seeds from \n Roadside and Adjacent Vegetation\n")+theme(plot.title=element_text(hjust=0.5),axis.title.x=element_text(face="bold",color="Black",size = 12),axis.title.y=element_text(face="bold",color="Black",size=12),legend.title=element_text(face="bold",size=10))
